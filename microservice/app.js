74a53db45cbf0738aea9ac8bf82c50c96edc30ca
Govardhanr2 <govardhan.r2@outlook.com>
2025-11-25 23:28:53 +0530

    Update app.js

--- microservice/app.js
+++ microservice/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Microservice');
+  res.end('Hello from G Microservice');
 });
 
 const port = process.env.PORT || 3000;
